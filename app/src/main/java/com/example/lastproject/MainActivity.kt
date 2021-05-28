470b40bd155e9edac52e7929ca80f99fddc67670
geoffroyhb <haftman-boutel@et.esiea.fr>
2021-05-28 17:33:34 +0200

    création d'une nouvelle branche pour faire des tests unitaire

--- app/src/main/java/com/example/lastproject/MainActivity.kt
+++ app/src/main/java/com/example/lastproject/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.lastproject
 
+
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
